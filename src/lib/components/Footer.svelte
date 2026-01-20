b0529b902bf47a30628c9624c5b2666553c7a25b
Pierre Prezelin <pierreprezelin.pro@gmail.com>
2026-01-20 14:10:52 -0500

    Add basic Header and Footer component with theme handling

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	const year = new Date().getFullYear();
+</script>
+
+<footer class="flex justify-center items-center">
+	<div class="container mx-auto">
+		<div class="relative w-full h-0.5 mt-24 mb-6 bg-pp-black rounded-full">
+			<span class="absolute top-[50%] left-[50%] w-4 h-4 block bg-pp-black border-4 border-pp-beige rounded-md rotate-45 -translate-1/2"></span>
+		</div>
+		<div class="py-9 text-sm text-center">
+			Pierre Prézelin © {year}
+		</div>
+		<div class="flex justify-center">
+			<span class="pattern bg-pp-blue first:rounded-tl-md"></span>
+			<span class="pattern bg-pp-brown"></span>
+			<span class="pattern bg-pp-yellow"></span>
+			<span class="pattern bg-pp-red last:rounded-tr-md"></span>
+		</div>
+	</div>
+</footer>
+
+<style lang="scss">
+	.pattern {
+		width: 1.25rem;
+		height: 1.25rem;
+	}
+</style>
---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+	import { Sun, Moon } from '@lucide/svelte';
+	import { mode, toggleMode } from 'mode-watcher';
+</script>
+
+<header class="flex items-center justify-between p-4">
+	<nav class="flex w-full">
+		<ul>
+			<li><a href="/works">Works</a></li>
+			<li><a href="/about">About</a></li>
+			<li><a href="/blog">Blog</a></li>
+		</ul>
+		<a href="/" title="Home" aria-label="Home">
+			<enhanced:img src="$lib/assets/logo.svg" alt="Logo" />
+		</a>
+		<ul>
+			<li>
+				<button type="button">FR</button>
+			</li>
+			<li>
+				<button type="button" onclick={toggleMode} aria-label="Toggle theme" class="theme-toggle">
+					<span class="theme-toggle-sun">
+						<Sun size="24" color="var(--color-pp-black)" />
+					</span>
+					<span class="theme-toggle-moon">
+						<Moon size="24" color="var(--color-pp-black)" />
+					</span>
+				</button>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<style lang="scss">
+	.theme-toggle {
+		&-moon {
+			display: none;
+		}
+		&-sun {
+			display: block;
+		}
+	}
+
+	:global(html.dark) {
+		.theme-toggle {
+		&-sun {
+			display: none;
+		}
+		&-moon {
+			display: block;
+		}
+	}
+	}
+
+	ul {
+		display: flex;
+		align-items: center;
+		gap: 2rem;
+		text-transform: uppercase;
+	}
+
+	a,
+	button {
+		position: relative;
+		font-family: var(--font-serif);
+
+		&::after {
+			position: absolute;
+			content: '';
+			left: 50%;
+			bottom: 0.5rem;
+			width: 0.25rem;
+			height: 0.25rem;
+			margin-left: -0.125rem;
+			background-color: var(--color-pp-black);
+			border-radius: 1px;
+			opacity: 0;
+		}
+
+		&:hover {
+			&::after {
+				opacity: 1;
+			}
+		}
+	}
+</style>
