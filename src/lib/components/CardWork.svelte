6d5b1e14d0e724cd71c0d37bf1e9a1fc98f05bf1
Pierre Prezelin <pierreprezelin.pro@gmail.com>
2026-02-02 14:08:46 -0500

    Add work index number

--- src/lib/components/CardWork.svelte
+++ src/lib/components/CardWork.svelte
@@ -4,10 +4,10 @@
 	import type { Work } from '$lib/types';
 	import { FolderSearch } from '@lucide/svelte';
 
-	let { work }: { work: Work } = $props();
+	let { work, displayedIndex }: { work: Work; displayedIndex: number } = $props();
 </script>
 
-<li class="flex flex-col items-center gap-5 mobile:flex-row tablet:gap-10">
+<li class="group relative flex flex-col items-center gap-5 mobile:flex-row tablet:gap-10">
 	<figure
 		class="h-56 w-full shrink-0 overflow-hidden rounded-xl border mobile:aspect-square mobile:h-full mobile:w-1/3 tablet:aspect-auto tablet:w-75"
 	>
@@ -31,7 +31,7 @@
 			{/if}
 		</a>
 	</figure>
-	<div class="flex grow flex-col w-full">
+	<div class="relative z-1 flex w-full grow flex-col">
 		<div class="flex items-center justify-between gap-2.5">
 			<h2 class="h5 mb-0.5">
 				<a href={localizeHref(`/works/${work.slug}`, { locale: getLocale() })}>{work.title}</a>
@@ -50,4 +50,9 @@
 			</a>
 		</p>
 	</div>
+	<span
+		class="absolute -right-1.5 -bottom-10 z-0 font-serif text-[91.56px] text-pp-yellow/10 transition-colors select-none group-hover:text-pp-yellow/20"
+	>
+		{displayedIndex.toString().padStart(2, '0')}
+	</span>
 </li>
